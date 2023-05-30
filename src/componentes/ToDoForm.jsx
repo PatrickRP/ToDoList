2f321c721dced70cb0f5ab7ff7f50e1e1a73023c
lucasluzz <lluz03@outlook.com>
2023-05-29 21:05:26 -0400

    Exibindo tarefa no console.log
    Futuramente esse retorno deve ser inserido no BD

--- src/componentes/ToDoForm.jsx
+++ src/componentes/ToDoForm.jsx
@@ -1,10 +1,15 @@
-import {useSate} from 'react'
+import {useState} from "react"
 
 const ToDoForm = () => {
+  const [value, setValue] = useState([])
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(value);
+  };
   return (
     <div className="form-toDo">
-        <form>
-            <input type="text" placeholder="Nova tarefa"></input>
+        <form onSubmit={submit}>
+            <input type="text" placeholder="Nova tarefa" onChange={(e) => setValue(e.target.value)}></input>
             <button className="add" type="submit">Adicionar</button>
         </form>
     </div>
