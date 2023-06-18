b12ded85e6ffd819a90f4323b051bf4bf0eb7b9f
Lucas Luz <89533457+lucasluzz@users.noreply.github.com>
2023-06-18 16:35:39 -0400

    Ajustes no CSS

--- src/App.js
+++ src/App.js
@@ -29,23 +29,25 @@ function App() {
       <h1>To-Do List</h1>
       <div className="todo-input">
         <input
+          className="todo-tarefa"
           type="text"
           value={text}
           onChange={(e) => setText(e.target.value)}
           placeholder="Enter a new task"
         />
-        <button onClick={addTodo}>Add</button>
+        <button onClick={addTodo}>Salvar</button>
+      
+        <ul className="todo-list">
+          {todos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              deleteTodo={deleteTodo}
+              toggleTodo={toggleTodo}
+            />
+          ))}
+        </ul>
       </div>
-      <ul className="todo-list">
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            deleteTodo={deleteTodo}
-            toggleTodo={toggleTodo}
-          />
-        ))}
-      </ul>
     </div>
   );
 }
